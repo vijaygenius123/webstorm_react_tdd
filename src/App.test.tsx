125f060ea5d3ccdefb047785b47541647a0e9e19
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-24 14:30:32 +0530

    Modified setupTests.ts and Added Simple Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import {shallow} from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders the heading', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('h1').text()).toBe('Hello React!');
+})
