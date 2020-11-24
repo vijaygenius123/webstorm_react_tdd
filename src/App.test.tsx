5acb44f8dc7ab88a3fdac5c52cc31aa4e41744cc
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-24 22:12:35 +0530

    Added more test cases for header

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import App from './App';
 import {shallow} from "enzyme";
 
-it('renders the heading', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('h1').text()).toBe('Hello React!');
+
+describe('Header', () => {
+    it('renders the heading text', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('h1').text()).toBe('Hello React!');
+
+    });
+
+    it('renders para text', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('p').text()).toBe('Nice TDD');
+    })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello React!</h1>
+            <p>Nice TDD</p>
+        </div>
+    );
 }
 
 export default App;
